d5d0a53b14120ec14ce85b0d2c56843890cc4c70
Ryu sichan <>
2023-01-31 00:42:45 +0900

    named constructor

--- dart/index.dart
+++ dart/index.dart
@@ -62,7 +62,7 @@
 //   print(name);
 // }
 
-// 6. Basic Data Types
+// 7. Basic Data Types
 // 아래 타입을 포함한 거의 대부분의 타입들이 객체로 이루어져 있다. (함수도 객체)
 // 이것이 Dart가 진정한 객체 지향 언어로 불리는 이유이다.
 // void main () {
@@ -72,7 +72,7 @@
 //   double money = 52.55;
 // }
 
-// 7. Lists
+// 8. Lists
 // dart의 유용한 점은 `collection if`와 `collection for`을 지원하는 것이다.
 
 // collection if를 사용하면 `존재할 수도 안할 수도 있는 요소를 가지고 올 수 있다.`
@@ -94,24 +94,24 @@
 //   ];
 // }
 
-// 8. String Interpolation
+// 9. String Interpolation
 // 변수 사용하는 방법
 
 // $달러 기호를 붙이고 사용할 변수를 적어주면 된다.
 // 만약 무언가를 계산하고 싶다면 ${ } 형태로 적어주면 된다.
-void main() {
-  var name = 'nico';
-  var age = 10;
-  var greeting = 'hello everyone, I\'m $name age is ${age + 10}';
-  print(greeting);
-}
+// void main() {
+//   var name = 'nico';
+//   var age = 10;
+//   var greeting = 'hello everyone, I\'m $name age is ${age + 10}';
+//   print(greeting);
+// }
 // String fetchAPI(int data) => data.toString();
 // void main(){
 //   final name = fetchAPI(123);
 //   print(name);
 // }
 
-// 자료 구조형 collection if 조건문
+// 10. 자료 구조형 collection if 조건문
 // 배열안에 직접적으로 if문을 사용할 수 있다.
 //   var giveMeFive = true;
 //   var numbers = [1, 2, 3, 4, if (giveMeFive) 5];
@@ -127,7 +127,7 @@ void main() {
 
 // print(newFriend);
 
-// Maps
+// 11. Maps
 
 // 일반적으로 맵은 key와 value를 연결하는 객체입니다. 키와 값 모두 모든 유형의 객체가 될 수 있습니다. 각 키는 한 번만 발생하지만 동일한 값을 여러 번 사용할 수 있습니다.
 //   var gifts = {
@@ -144,15 +144,15 @@ void main() {
 //   gifts2['fifth'] = 'golden rings';
 // https://dart.dev/guides/language/language-tour#maps
 
-// Functions
+// 12. Functions
 
 // Dart는 진정한 객체 지향 언어이므로 함수도 객체이며 타입이 Function입니다. 이는 함수를 변수에 할당하거나 다른 함수에 인수로 전달할 수 있음을 의미합니다.
 // 하나의 표현식만 포함하는 함수의 경우 아래와 같이 단축 구문을 사용할 수 있습니다.
-String sayHello(String name) => "Hello ${name} nice to meet you.";
+// String sayHello(String name) => "Hello ${name} nice to meet you.";
 
-num plus(num a, num b) => a + b;
+// num plus(num a, num b) => a + b;
 
-// Named Parameters
+// 13. Named Parameters
 
 // parameter에 이름을 붙여서 사용할 수 있다. 작동하는 function의 parameter에 {}를 씌워주면
 // 순서에 관계없이 parameter의 name을 활용할수 있다
@@ -169,3 +169,64 @@ num plus(num a, num b) => a + b;
 //     age: 10
 //   ));
 // }
+
+// 14. Named Constructor Parameters
+
+// 말그대로 constructor의 parameter값에다가 named처리를 해줘서
+// 필요시 함수로 호출할때 named로 받아서 처리하는 방법이다.
+// !required는 nullsafety를 받쳐주기 위한 중요한 요소이다
+
+// class Player {
+//   String name;
+//   int age;
+//   String description;
+
+//   Player({required this.name, required this.age, required this.description});
+// }
+
+// void main() {
+//   var myTeam = Player(name: "hi", age: 17, description: "happy");
+//   print(myTeam);
+// }
+
+// 15. Named constructor
+// 생성자(constructor)함수를 여러개 만들고 싶다면 어떨까?
+
+class Team {
+  String name;
+  int age;
+  String value;
+  int xp;
+
+  Team(
+      {required this.name,
+      required this.age,
+      required this.value,
+      required this.xp});
+
+// named parameter
+  Team.redTeam({
+    required String name,
+    required int age,
+    // :부등호를 써줌으로써 default값을 정해줌
+  })  : this.age = age,
+        this.name = name,
+        this.value = "blue",
+        this.xp = 0;
+
+// position parameter
+  Team.blueTeam(String name, int age)
+      : this.age = age,
+        this.name = name,
+        this.value = 'red',
+        this.xp = 0;
+}
+
+void main() {
+  var myTeam = Team(name: "1", age: 12, value: "24", xp: 10);
+  var redTeam = Team.redTeam(name: "hijun", age: 44);
+  var blueTeam = Team.blueTeam("juno", 42);
+  print(myTeam);
+  print(redTeam);
+  print(blueTeam);
+}
