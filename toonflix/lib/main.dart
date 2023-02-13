74936419ba549f8ce675bddb94e474fb0348a1d7
Ryusichan <Ryusichan@github.com>
2023-02-13 16:12:03 +0900

    life cycle 이해

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -22,16 +22,38 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   //버튼을 활용하여 state의 값을 바꾸기
   int value = 5800200;
+  bool val = true;
 
+// life cycle에 대한이해 값이 바뀔때 initialState 변경 => build 변경
   void addCounter() {
     setState(() {
       value = value + 1;
     });
   }
 
+  void toggleVal() {
+    setState(() {
+      val = !val;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+// dispose method 위젯이 스크린에서 제거될때 사용되는요소이다
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
   // build를 클래스 안에 넣어주는 약속을 해준다
   @override
   Widget build(BuildContext context) {
+    print('build');
     return MaterialApp(
       theme: ThemeData(
           textTheme: const TextTheme(
@@ -82,13 +104,15 @@ class _MyAppState extends State<MyApp> {
                       fontWeight: FontWeight.w300),
                 ),
                 const SizedBox(height: 4),
-                Text(
-                  '\$ $value',
-                  style: const TextStyle(
-                      fontSize: 36,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
+                val
+                    ? Text(
+                        '\$ $value',
+                        style: const TextStyle(
+                            fontSize: 36,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w600),
+                      )
+                    : const Text(''),
                 const SizedBox(
                   height: 20,
                 ),
@@ -105,7 +129,7 @@ class _MyAppState extends State<MyApp> {
                         textColor: Color.fromARGB(255, 255, 255, 255)),
                     //버튼을 활용하여 state의 값을 바꾸기
                     IconButton(
-                        onPressed: addCounter,
+                        onPressed: toggleVal,
                         icon: const Icon(Icons.add_a_photo)),
                   ],
                 ),
